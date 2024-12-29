b8717b6d3cdbbcfbb5b6292415122452b111ebd1
ManyFlez162 <yarvis42@gmail.com>
2024-12-29 16:57:47 -0700

    dividing project in back and front

--- devTree-backend/src/index.ts
+++ devTree-backend/src/index.ts
@@ -4,5 +4,6 @@ import server from './server'
 const port = process.env.port || 3000 // Usa el puerto del host en el deploy. Si es local, utiliza el puerto 3000
 
 server.listen(port, () => {
+    // si
     console.log(colors.blue.bold('Servidor corriendo en: '), port)
 })
