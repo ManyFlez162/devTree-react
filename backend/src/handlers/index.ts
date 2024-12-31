afb611d0846cea0f20d53436242c3ae0fb516bf1
ManyFlez162 <yarvis42@gmail.com>
2024-12-30 18:45:40 -0700

    jwt applied

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -3,6 +3,7 @@ import { validationResult } from "express-validator"
 import slug from "slug"
 import User from "../models/User"
 import { checkPassword, hashPassword } from "../utils/auth"
+import { generateJWT } from "../utils/jwt"
 
 export const createAccount = async ( req: Request, res: Response): Promise<any> => {
 
@@ -55,5 +56,7 @@ export const login = async (req: Request, res: Response): Promise<any> => {
         return res.status(401).json({error : error.message})
     }
 
-    res.send("Autenticado...")
+    const token = generateJWT({id: user._id})
+
+    res.send(token)
 }
